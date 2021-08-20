7d7cddf0116e6eddeef921c7dadd6143cb389bfb
David Puzey <david.puzey@gmail.com>
2021-08-20 17:29:33 +0100

    Rename, allow exact filtering, make dates iso format, and improve readme

--- modules/bins/README.md
+++ modules/bins/README.md
@@ -1,3 +1,20 @@
 Find collection dates: https://my.portsmouth.gov.uk/en/AchieveForms/?form_uri=sandbox-publish://AF-Process-26e27e70-f771-47b1-a34d-af276075cede/AF-Stage-cd7cc291-2e59-42cc-8c3f-1f93e132a2c9/definition.json
 
+UPRN's: https://www.gov.uk/government/publications/open-standards-for-government/identifying-property-and-street-information
 
+Usage:
+`./pcc_bin_dates.py <postcode> [<filter>] [exact]`
+postcode - The postcode to search for
+filter (optional) - Used to filter down to a single address, if the filter matches multiple entries all entries will be listed and the filter will need to be made more specific
+exact (optional) - Just the word 'exact', if it's the 3rd argument then the filter is used as an exact match for an address. Not always needed, but some addresses (for example number 1) could match multiple address on a street (from same example, numbers 11, 21, 31, etc) so a partial match would just list addresses rather than collection dates
+If filter is not included all addresses at the postcode are listed.
+I ususally just use my house number for the filter
+
+If there are multiple addresses available then a list of addresses is output, if a single address is found then bin collection dates for general rubbish and recycling is returned in JSON format.
+
+Example (not an actual address, just an example of how it could be used):
+List all addresses: `./pcc_bin_dates.py "PO1 1AA"`
+Output collection dates: `./pcc_bin_dates.py "PO1 1AA" 73`
+
+
+Note: It can be a little slow to retrieve dates because PCC website is slow
--- modules/bins/pcc_bin_dates.py
+++ modules/bins/pcc_bin_dates.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# Get list of bin collection dates from Portsmouth Council website
-# TODO This is a big on the hacky side, not really robust at all, we should probably check that requests have completed properly rather than just assuming and just generally do everything better
+# Get list of bin collection dates from Portsmouth City Council website
+# TODO This is a bit on the hacky side, not really robust at all, we should probably check that requests have completed properly rather than just assuming and just generally do everything better
 #      FYI The reply from runLookup has a 'status' field that should be set to 'done', maybe use this??? or it might be nothing, also check for HTTP code 200 maybe????
 # Notes:
 #   - I noticed a CSRF floating around the website, if things go wrong we might need to use this as well
@@ -15,7 +15,7 @@ from datetime import datetime
 
 class PortsmouthCouncilAPI:
     """ Interact with PCCs website API """
-    def __init__(self, host='my.portsmouth.gov.uk'):
+    def __init__(self, host='my.portsmouth.gov.uk'):  # Note: Host is configurable because it looks like PCC website is based on a third party system so this stuff might work with other websites ... maybe
         """ Create connection to host
         host - This is configurable on the off chance this can be used with another councils website
         """
@@ -60,15 +60,20 @@ class PortsmouthCouncilAPI:
         resp = self.conn.getresponse()
         return json.loads(resp.read())
 
-    def lookup_uprn(self, postcode, filter=''):
+    def lookup_uprn(self, postcode, filter='', exact=False):
         """ Get list of addresses and UPRNs
         postcode - Postcode to lookup
         filter - String used to filter addresses. Only addresses that contain this string are returned. Defaults to an empty string
+        exact - Whether the filter should be treated as an exact match, best used for low number addresses (e.g. number 1 could match 11, 21, 31 etc easily too). Defaults to False
         Returns list of addresses and UPRNs
         """
         addr_data = self.lookup('58ca773b44b4b', {'postcode_search_after': postcode})
         addr_data = addr_data['integration']['transformed']['select_data']
-        return [{'addr': item['label'], 'uprn': item['value']} for item in addr_data if filter in item['label']]
+        if exact:
+            addr = [{'addr': item['label'], 'uprn': item['value']} for item in addr_data if filter == item['label']]
+        else:
+            addr = [{'addr': item['label'], 'uprn': item['value']} for item in addr_data if filter in item['label']]
+        return addr
 
     def lookup_bins(self, uprn):
         """ Get list of bin collection dates from UPRN)
@@ -79,20 +84,23 @@ class PortsmouthCouncilAPI:
         dates_data = dates_data['integration']['transformed']['rows_data']['0']
         rubbish = dates_data['listRefDatesHTML'].split('<p>')[0].split('<br />')
         recycling = dates_data['listRecDatesHTML'].split('<p>')[0].split('<br />')
-        rubbish = [datetime.strptime(date.strip('*'), '%A %d %B %Y').timestamp() for date in rubbish if date != '']
-        recycling = [datetime.strptime(date.strip('*'), '%A %d %B %Y').timestamp() for date in recycling if date != '']
+        rubbish = [datetime.strptime(date.strip('*'), '%A %d %B %Y').date().isoformat() for date in rubbish if date != '']
+        recycling = [datetime.strptime(date.strip('*'), '%A %d %B %Y').date().isoformat() for date in recycling if date != '']
         return {'rubbish': rubbish, 'recycling': recycling}
 
-    def get_bin_dates(self, postcode, filter):
+    def get_bin_dates(self, postcode, filter='', exact=False):
         """ Get list of bin collection dates
         postcode - Postcode to lookup
         filter - String used to filter addresses. Only addresses that contain this string are returned. Defaults to an empty string
+        exact - Whether the filter should be treated as an exact match, best used for low number addresses (e.g. number 1 could match 11, 21, 31 etc easily too). Defaults to False
         Returns dictionary containing list of collection dates for General Rubbish and Recycling
         Bin Collection Date Lookup: https://my.portsmouth.gov.uk/en/AchieveForms/?form_uri=sandbox-publish://AF-Process-26e27e70-f771-47b1-a34d-af276075cede/AF-Stage-cd7cc291-2e59-42cc-8c3f-1f93e132a2c9/definition.json
         """
-        uprn = self.lookup_uprn(postcode, filter)
+        uprn = self.lookup_uprn(postcode, filter, exact)
         if len(uprn) > 1:
-            raise Exception(f'Multiple addresses found at postcode "{postcode}" for filter "{filter}":\n ' + '\n '.join([addr['addr'] for addr in uprn]))
+            raise Exception(f'Multiple addresses found at postcode "{postcode}" for filter "{filter}":\n ' + '\n '.join([repr(addr['addr']) for addr in uprn]))
+        if len(uprn) == 0:
+            raise Exception(f'Address not found')
         uprn = uprn[0]['uprn']
         return self.lookup_bins(uprn)
 
@@ -103,10 +111,14 @@ if __name__ == '__main__':
         filter = sys.argv[2]
     else:
         filter = ''
+    if len(sys.argv) >= 4 and sys.argv[3] == 'exact':
+        exact = True
+    else:
+        exact = False
 
     try:
         api = PortsmouthCouncilAPI()
-        dates = api.get_bin_dates(postcode, filter)
+        dates = api.get_bin_dates(postcode, filter, exact)
         print(json.dumps(dates))
     except Exception as err:
         print(err)
